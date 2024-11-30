a7685339a2433aa5f1a22780140f2f0268bafe33
matsuminaoya <paintabridge@gmail.com>
2024-11-30 12:12:49 +0000

    2024年11月30日 21:12

--- ver241130a/main1.py
+++ ver241130a/main1.py
@@ -1148,10 +1148,10 @@ work = 2
 g_step = 2
 ani_step = 2
 
-# ok start(lorf="leave",ininet="full", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=trial, work=work)
-# start(lorf="leave",ininet="full", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="eleven", trial=trial, work=work)
+start(lorf="leave",ininet="full", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=trial, work=work)
+start(lorf="leave",ininet="full", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="eleven", trial=trial, work=work)
 start(lorf="form",ininet="null", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="zero", trial=trial, work=work)
-start(lorf="form",ininet="random", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="zero", trial=trial, work=work)
+start(lorf="both",ininet="ba", tcinivalue="random", tlinivalue="random", tfinivalue="random", trial=trial, work=work)
 
 ###note-1123まで
 # np.array([0,0],[0,0])でリストからナンパイ
