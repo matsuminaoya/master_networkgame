0397ea1ccd426d538ab7787a7352923d7eb4905f
expiater2025 <paintabridge@gmail.com>
2024-12-05 23:48:58 +0000

    2024年12月6日 8:48

--- src/src1/x241205_silver/main4.py
+++ src/src1/x241205_silver/main4.py
@@ -1295,7 +1295,7 @@ def start(lorf = "lorf", ininet = "ininet", tcinivalue = "tcinivalue", tlinivalu
     elif lorf == "both":
         df = pd.DataFrame({"tr":tr_trs_repeat, "ge":ge_ges_repeat, "tc":tc_all_ges_trs, "tl":tl_all_ges_trs, "tf":tf_all_ges_trs, "ln":ln_all_ges_trs, "dn":dn_all_ges_trs})
     df.to_csv(name+"/"+name+"_all.csv")#フォルダの中に格納
-    df = Graph_all_dfgstep(name+"/"+name+"_all.csv")#フォルダの中に格納
+    #df = Graph_all_dfgstep(name+"/"+name+"_all.csv")#TODO:
     # time2 = time.time()
     # Graph_all_vio(df, ylabel="tc").savefig(name + "_all_vio_tc.png")
     # time3 = time.time()
@@ -1304,16 +1304,16 @@ def start(lorf = "lorf", ininet = "ininet", tcinivalue = "tcinivalue", tlinivalu
     # Graph_all_vio(df, ylabel="ln").savefig(name + "_all_vio_ln.png")
     # time4 = time.time()
     #box graph
-    Graph_all_box(df, ylabel="tc", name=name)
-    Graph_all_box(df, ylabel="ln", name=name)
-    Graph_all_box(df, ylabel="dn", name=name)
-    if lorf == "leave":
-        Graph_all_box(df, ylabel="tl", name=name)
-    elif lorf == "form":
-        Graph_all_box(df, ylabel="tf", name=name)
-    elif lorf == "both":
-        Graph_all_box(df, ylabel="tl", name=name)
-        Graph_all_box(df, ylabel="tf", name=name)
+    # Graph_all_box(df, ylabel="tc", name=name)TODO:
+    # Graph_all_box(df, ylabel="ln", name=name)
+    # Graph_all_box(df, ylabel="dn", name=name)
+    # if lorf == "leave":
+    #     Graph_all_box(df, ylabel="tl", name=name)
+    # elif lorf == "form":
+    #     Graph_all_box(df, ylabel="tf", name=name)
+    # elif lorf == "both":
+    #     Graph_all_box(df, ylabel="tl", name=name)
+    #     Graph_all_box(df, ylabel="tf", name=name)
     #network gif
     #df = pd.DataFrame({"ge":ge_ges, "linkmatrix":linkmatrix_ges_tr0})
     #df.to_csv(name + "_tr0_network.csv")
@@ -1348,15 +1348,15 @@ start(lorf="both",ininet="null", tcinivalue="zero", tlinivalue="zero", tfinivalu
 start(lorf="form",ininet="null", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=1, work=1000)
 start(lorf="leave",ininet="full", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=1, work=1000)
 
-# start(lorf="both",ininet="full", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=10, work=1000)
-# start(lorf="both",ininet="null", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=10, work=1000)
-# start(lorf="form",ininet="null", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=10, work=1000)
-# start(lorf="leave",ininet="full", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=10, work=1000)
+start(lorf="both",ininet="full", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=10, work=1000)
+start(lorf="both",ininet="null", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=10, work=1000)
+start(lorf="form",ininet="null", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=10, work=1000)
+start(lorf="leave",ininet="full", tcinivalue="zero", tlinivalue="zero", tfinivalue="zero", trial=10, work=1000)
 
-# start(lorf="both",ininet="full", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="eleven", trial=10, work=1000)
-# start(lorf="both",ininet="null", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="eleven", trial=10, work=1000)
-# start(lorf="form",ininet="null", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="eleven", trial=10, work=1000)
-# start(lorf="leave",ininet="full", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="eleven", trial=10, work=1000)
+start(lorf="both",ininet="full", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="eleven", trial=10, work=1000)
+start(lorf="both",ininet="null", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="eleven", trial=10, work=1000)
+start(lorf="form",ininet="null", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="eleven", trial=10, work=1000)
+start(lorf="leave",ininet="full", tcinivalue="eleven", tlinivalue="eleven", tfinivalue="eleven", trial=10, work=1000)
 
 ###note-1123まで
 # np.array([0,0],[0,0])でリストからナンパイ
